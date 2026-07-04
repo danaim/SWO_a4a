1358142a869135fc49cc8e2c95b825a154de63f8
danaim <mantopoulou.danai@gmail.com>
2026-07-04 18:52:56 +0200

    some more info in the update run

--- analysis/index_preparation.R
+++ analysis/index_preparation.R
@@ -11,38 +11,38 @@ idx_bio <- FLIndices()
 
 # GRC
 dms <- list(age="all", year=1987:2024)
-idx_bio[['GRC_LL']] <- FLIndexBiomass(index = FLQuant(indices$GRC_LL_std[indices$Year %in% 1987:2024], 
+idx_bio[['GR_LL']] <- FLIndexBiomass(index = FLQuant(indices$GRC_LL_std[indices$Year %in% 1987:2024], 
   dimnames=dms))
 
 # LIG early
 dms <- list(age="all", year=1991:2009)
-idx_bio[['LIG_LL_early']] <- FLIndexBiomass(index = FLQuant(indices$w_LIG_LL_early_std[indices$Year %in% 1991:2009], 
+idx_bio[['LI_SUR']] <- FLIndexBiomass(index = FLQuant(indices$w_LIG_LL_early_std[indices$Year %in% 1991:2009], 
   dimnames=dms))
 
 # LIG
 dms <- list(age="all", year=2009:2022)
-idx_bio[['LIG_LL']] <- FLIndexBiomass(index = FLQuant(indices$w_LIG_LL_std[indices$Year %in% 2009:2022], 
+idx_bio[['LI_LL']] <- FLIndexBiomass(index = FLQuant(indices$w_LIG_LL_std[indices$Year %in% 2009:2022], 
   dimnames=dms))
 
 # SPN
 dms <- list(age="all", year=1988:2023)
-idx_bio[['SPN_LL']] <- FLIndexBiomass(index = FLQuant(indices$w_SPN_LL_std[indices$Year %in% 1988:2023], 
+idx_bio[['SP_LL']] <- FLIndexBiomass(index = FLQuant(indices$w_SPN_LL_std[indices$Year %in% 1988:2023], 
   dimnames=dms))
 
 # MOR
 dms <- list(age="all", year=2012:2024)
-idx_bio[['MOR_LL']] <- FLIndexBiomass(index = FLQuant(indices$MOR_LL_std[indices$Year %in% 2012:2024], 
+idx_bio[['MO_LL']] <- FLIndexBiomass(index = FLQuant(indices$MOR_LL_std[indices$Year %in% 2012:2024], 
   dimnames=dms))
 
 for(i in 1:length(idx_bio)) {
   range(idx_bio[[i]],c("startf","endf")) = c(0,1)
-  range(idx_bio[[i]],c("min","max")) = c(1,3)
+  range(idx_bio[[i]],c("min","max")) = c(2,4)
   units(index(idx_bio[[i]])) <- "t"
 }
 idx <- idx_bio
-# add Ligurian old index that is missing
+# add Sicilian old index that is missing
 load("2020_files/runs/a4a/MCMC/input4MCMC.RData")
-idx[['SIC_LL']] <- idx_bio[['SI_LL']]
+idx[['SI_LL']] <- idx_bio[['SI_LL']]
 
 saveRDS(idx, file = 'Robj/swo_bio_idx.rds')
 
